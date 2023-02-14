90ce9495ba3c0881db2113d19b26f0b5ab098e44
Aaron Anderson <aace2@me.com>
2023-02-14 09:12:59 -0800

    Initial Commit

---
+++ BringTheNoise/BringTheNoiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  BringTheNoiseApp.swift
+//  BringTheNoise
+//
+//  Created by TheForce on 2/14/23.
+//
+
+import SwiftUI
+
+@main
+struct BringTheNoiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
